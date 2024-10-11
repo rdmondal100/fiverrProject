aaac7ba06963919b15f9d27d8e6efdbda3b55278
Riday <rdmondal100@gmail.com>
2024-10-11 15:54:10 +0600

    finish the initial task of the pages

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+// To handle the menubar
+
 document.addEventListener('DOMContentLoaded', function() {
   const hamburger = document.getElementById('hamburger');
   const navLinks = document.querySelector('.nav-links');
@@ -6,5 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
       navLinks.classList.toggle('active');
       // Optional: Toggle the hamburger icon if needed
       hamburger.classList.toggle('active');
+  });
+});
+
+
+// To handle the current active menue 
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPage = window.location.pathname.split("/").pop();
+  console.log(currentPage)
+  const navLinks = document.querySelectorAll(".nav-links ul li a");
+  console.log(navLinks)
+
+  navLinks.forEach(link => {
+      if (link.getAttribute("href") === currentPage) {
+          link.classList.add("active");
+      }
   });
 });
